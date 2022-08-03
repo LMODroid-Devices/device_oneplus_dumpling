382de041c4a1776e228135d5e20dc4db0d99ce61
Erfan Abdi <erfangplus@gmail.com>
2022-08-04 01:43:31 +0430

    Adapt to LMODroid by device-add script
    Change-Id: I7b4a81e490dc82bebd0a7b7b16fa06920da6478d

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/lmodroid_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    lmodroid_dumpling-user \
+    lmodroid_dumpling-userdebug \
+    lmodroid_dumpling-eng
